d1f8a9350d8163147ce9da6e0427d553f8e4e9dc
André de Moraes Barros <decomoraes@Andres-MacBook-Pro.local>
2025-05-02 20:36:36 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{ItemFn, parse_macro_input};
+use syn::parse_macro_input;
+use syn::{FnArg, ItemFn, PatType};
 
 #[proc_macro_attribute]
 pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(item as ItemFn);
     let fn_name = &input_fn.sig.ident;
+
+    let arg_ty = input_fn
+        .sig
+        .inputs
+        .iter()
+        .find_map(|arg| {
+            if let FnArg::Typed(PatType { ty, .. }) = arg {
+                Some(&**ty)
+            } else {
+                None
+            }
+        })
+        .expect("Function must have one argument");
 
     let expanded = quote! {
         #input_fn
@@ -27,9 +41,8 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
         #[unsafe(no_mangle)]
         pub extern "C" fn execute(retptr: *mut u32, ptr: *const u8, len: usize) {
             let data = unsafe { std::slice::from_raw_parts(ptr, len) };
-            if let Ok(json_out) = serde_json::from_slice::<serde_json::Value>(data) {
-                let output = #fn_name(json_out);
-                let mut bytes = serde_json::to_vec(&output).expect("serialize failed");
+            if let Ok(val) = serde_json::from_slice::<#arg_ty>(data) {
+                let mut bytes = serde_json::to_vec(&#fn_name(val)).expect("serialize failed");
                 bytes.shrink_to_fit();
                 unsafe {
                     *retptr.offset(0) = bytes.as_ptr() as u32;
@@ -47,5 +60,6 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
             std::mem::forget(bytes);
         }
     };
+
     TokenStream::from(expanded)
 }
